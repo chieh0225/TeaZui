1d7a4380d030f7171f9298a455dffaaa7444803b
chieh <secret12501250@gmail.com>
2024-09-17 11:39:22 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import feather from "feather-icons";
+feather.replace(); // 直接替換圖標
+
+console.log("Hello world!");
